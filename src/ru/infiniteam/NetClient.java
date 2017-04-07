9dd824dd7687fadebb551e12bb7f9082e36761e3
Boris Timofeenko <Boris Timofeenko>
2017-04-07 23:58:44 +1000

    Basic network 2

--- src/ru/infiniteam/NetClient.java
+++ src/ru/infiniteam/NetClient.java
@@ -1,4 +1,8 @@
 package ru.infiniteam;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -6,13 +10,32 @@ import java.net.Socket;
  * Created by t1meshft on 07.04.2017.
  */
 public class NetClient {
-   // public static final int PORT = 8080;
+    protected String address;
+    protected int port;
+    protected Socket socket;
+    protected InetAddress ipAddress;
+    protected InputStream sin;
+    protected OutputStream sout;
+    DataInputStream in;
+    DataOutputStream out;
 
     NetClient(String address, final int PORT){
-        try{
-            InetAddress ipAddress = InetAddress.getByName(address);
-            Socket socket = new Socket(ipAddress, PORT);
-            System.out.println("Connected");
-        } catch(Exception x) { x.printStackTrace(); throw new Error();}
+        this.address = address;
+        this.port = PORT;
     }
+
+    public int Connect(){
+        try{
+            this.ipAddress = InetAddress.getByName(this.address);
+            this.socket = new Socket(this.ipAddress, this.port);
+            this.sin = this.socket.getInputStream();
+            this.sout = this.socket.getOutputStream();
+            this.in = new DataInputStream(this.sin);
+            this.out = new DataOutputStream(this.sout);
+            System.out.println("Connected");
+            return(0);
+        } catch (Exception x) {return(-1);}
+    }
+
+
 }
--- src/ru/infiniteam/NetServer.java
+++ src/ru/infiniteam/NetServer.java
@@ -1,4 +1,9 @@
 package ru.infiniteam;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -6,14 +11,30 @@ import java.net.Socket;
  * Created by t1meshft on 07.04.2017.
  */
 public class NetServer {
-    //public static final int PORT = 8080;
+    protected int port;
+    protected ServerSocket ss;
+    protected Socket socket;
+    protected InputStream sin;
+    protected OutputStream sout;
+    DataInputStream in;
+    DataOutputStream out;
+
 
     NetServer(final int PORT){
+        this.port = PORT;
+    }
+
+    public int Establish(){
         try {
-            ServerSocket ss = new ServerSocket(PORT);
+            this.ss = new ServerSocket(this.port);
             System.out.println("Waiting for a client...");
-            Socket socket = ss.accept();
+            this.socket = this.ss.accept();
             System.out.println("Got a client");
-        } catch(Exception x) { x.printStackTrace(); throw new Error();}
+            this.sin = this.socket.getInputStream();
+            this.sout = this.socket.getOutputStream();
+            this.in = new DataInputStream(this.sin);
+            this.out = new DataOutputStream(this.sout);
+            return (0);
+        } catch(Exception x) {return (-1);}
     }
 }
